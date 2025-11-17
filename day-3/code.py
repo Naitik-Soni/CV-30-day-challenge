563c97b7c40621152c8738517607cfede10031d0
Naitik Soni <naitiksoni1705@gmail.com>
2025-11-17 18:52:24 +0530

    Added code for opening and closing

--- day-3/code.py
+++ day-3/code.py
@@ -1,41 +1,35 @@
 import cv2
 import numpy as np
 
-image_path = r"P:\Computer vision Experiments\30-Days challenge\CV-30-day-challenge\Images\full moon.jpg"
-og = cv2.imread(image_path)
+# Read image
+img_path = r"P:\Computer vision Experiments\30-Days challenge\CV-30-day-challenge\Images\deer.jpg"
+img = cv2.imread(img_path, 0)
 
-image = cv2.cvtColor(og, cv2.COLOR_BGR2GRAY)
+resizefactor = 0.7
+h, w = img.shape[:2]
+img = cv2.resize(img, (int(w*resizefactor), int(h*resizefactor)))
 
-ret, thresholded = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# Define structuring element
+kernel = np.ones((5,5),np.uint8)
 
-kernel = np.ones((5,5), dtype=np.uint8)
+# Perform dilation
+dilation = cv2.dilate(img, kernel, iterations=1)
 
-eroded = cv2.erode(thresholded, kernel, 9)
-diluted = cv2.dilate(eroded, kernel, 11)
+# Perform erosion
+erosion = cv2.erode(img, kernel, iterations=1)
 
-mask = cv2.bitwise_not(diluted)
+# Opening
+opened = cv2.dilate(erosion, kernel, iterations=1)
 
-final_image = cv2.bitwise_and(og, og, mask=mask, )
+# Closing
+closed = cv2.erode(dilation, kernel, iterations=1)
 
-lower_black = np.array([0, 0, 0])
-upper_black = np.array([0, 0, 0])
-black_mask = cv2.inRange(final_image, lower_black, upper_black)
-
-b,g,r = cv2.split(final_image)
-
-alpha = np.ones(b.shape, dtype=b.dtype) * 255
-alpha[black_mask == 255] = 0
-alpha[black_mask != 255] = 255
-
-new_final_image = cv2.merge((b,g,r, alpha))
-
-
-# cv2.imshow("Dilated", diluted)
-# cv2.imshow("Eroded", eroded)
-# cv2.imshow("Binary", thresholded)
-# cv2.imshow("Image", image)
-
-cv2.imwrite("outputs/final.png", new_final_image)
+# Display results
+cv2.imshow('Original',img)
+cv2.imshow('Dilation',dilation)
+cv2.imshow("Eroded", erosion)
+cv2.imshow("Opened", opened)
+cv2.imshow("Closed", closed)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
