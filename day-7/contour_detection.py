bc6ec34973ed201140174eacd99ae36fee94bb99
Naitik Soni <soni.naitik>
2025-11-27 18:44:31 +0530

    Added images for contour detection

--- day-7/contour_detection.py
+++ day-7/contour_detection.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread(r"..\Images\elephant.jpg")
+img = cv2.imread(r"..\Images\rings.webp")
 
 h, w = img.shape[:2]
 resize_factor = 1
@@ -9,14 +9,14 @@ img = cv2.resize(img, (int(w * resize_factor), int(h * resize_factor)))
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # gray = cv2.equalizeHist(gray)
 
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
+blur = cv2.GaussianBlur(gray, (11, 11), 0)
 ret, thresh = cv2.threshold(blur, 127, 255, cv2.THRESH_BINARY_INV)
 
 contours, _ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
 print("Length of contours:", len(contours))
 
-cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+cv2.drawContours(img, contours, -1, (127, 180, 255), 3)
 for cnt in contours:
 
     peri = cv2.arcLength(cnt, True)
