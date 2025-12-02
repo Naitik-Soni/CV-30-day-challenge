202500e05a536e1ef443e257e51ccfbedd54ba27
Naitik Soni <naitiksoni1705@gmail.com>
2025-12-02 17:41:49 +0530

    Changes in code for number detection

--- Practical tasks day (1-7)/Task-10/task.py
+++ Practical tasks day (1-7)/Task-10/task.py
@@ -1,17 +1,17 @@
 import cv2
 from number_recognizer import recognize_number
 
-imgpath = 'sudoku-1.png'
+imgpath = 'sudoku.webp'
 img = cv2.imread(imgpath)
 
-resize_factor = 0.75
+resize_factor = 0.15
 h, w = img.shape[:2]
 new_size = (int(w * resize_factor), int(h * resize_factor))
 resized_img = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
 
 gray = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
 
-thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)[1]
+thresholded = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY_INV)[1]
 
 contours, _ = cv2.findContours(thresholded, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 # cv2.drawContours(resized_img, contours, -1, (0, 255, 0), 2)
@@ -22,7 +22,7 @@ for cnt in contours:
     area = cv2.contourArea(cnt)
     peri = cv2.arcLength(cnt, True)
     approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
-    print("Area:", area, "Vertices:", len(approx))
+    # print("Area:", area, "Vertices:", len(approx))
 
     if area < 2500 and len(approx) == 4 and area  > 500:
         bounding_rect.append(cv2.boundingRect(approx))
