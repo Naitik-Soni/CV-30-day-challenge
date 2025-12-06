7a8017d681250582a8a3c0a02ac589adf03a1aed
Naitik Soni <naitiksoni1705@gmail.com>
2025-12-06 18:46:29 +0530

    Added modular code

--- day-10/face_blending.py
+++ day-10/face_blending.py
@@ -1,25 +1,47 @@
 import cv2
 import numpy as np
 
-image1 = cv2.imread(r"../Images/Face.jpg")
-image2 = cv2.imread(r"../Images/city.jpg")
+def load_resize_images():
+    image1 = cv2.imread(r"../Images/Face.jpg")
+    image2 = cv2.imread(r"../Images/city.jpg")
 
-image1 = cv2.resize(image1, (600, 600), cv2.INTER_AREA)
-image2 = cv2.resize(image2, (600, 600), cv2.INTER_AREA)
+    image1 = cv2.resize(image1, (600, 600), cv2.INTER_AREA)
+    image2 = cv2.resize(image2, (600, 600), cv2.INTER_AREA)
 
-gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
-gray2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    return image1, image2
 
-alpha = 0.5
-# image1[gray1>250] = [0,255,0]
-blended_image = cv2.addWeighted(image1, alpha, image2, 1-alpha, 0)
-selected_blended_pixels = blended_image[gray1 < 251]
-image1[gray1 < 251] = selected_blended_pixels
+def return_gray_images(images):
+    gray1 = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
+    gray2 = cv2.cvtColor(images[1], cv2.COLOR_BGR2GRAY)
 
-cv2.imshow("Image 1", image1)
-cv2.imshow("Image 2", image2)
-cv2.imshow("Gray 1", gray1)
-cv2.imshow("Gray 2", gray2)
+    return gray1, gray2
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def simple_blend(images, grays):
+    alpha = 0.5
+    # image1[gray1>250] = [0,255,0]
+    blended_image = cv2.addWeighted(images[0], alpha, images[1], 1-alpha, 0)
+    selected_blended_pixels = blended_image[grays[0] < 251]
+    images[0][grays[0] < 251] = selected_blended_pixels
+
+    return images
+
+def blur_image(image):
+    return cv2.GaussianBlur(image, (5,5), 0)
+
+def main():
+    image1, image2 = load_resize_images()
+    gray1, gray2 = return_gray_images((image1, image2))
+
+    blurred = blur_image(gray1)
+    image1, image2 = simple_blend((image1, image2), (blurred, gray2))
+
+
+    cv2.imshow("Image 1", image1)
+    cv2.imshow("Image 2", image2)
+    cv2.imshow("Gray 1", gray1)
+    cv2.imshow("Blurred", blurred)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+main()
