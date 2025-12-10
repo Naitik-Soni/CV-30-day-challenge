51eb92d40c6d519997826e7c7b9127655f041c4b
Naitik Soni <naitiksoni1705@gmail.com>
2025-12-10 13:13:48 +0530

    Added code for panorama stitching by own and by chatgpt

---
+++ day-15/panorama_creation.py
@@ -0,0 +1,79 @@
+from panorama_utils import *
+
+resize_factor = 0.2
+
+def get_homography_image(prev_img, img2, final_width):
+    # image1 = load_image(img1, True, resize_factor)
+    image2 = load_image(img2, True, resize_factor)
+
+    kp1, dst1 = get_features(prev_img)
+    kp2, dst2 = get_features(image2)
+
+    good_matches = get_good_matches(dst1, dst2)
+
+    src_kp, dst_kp = get_final_points(kp1, kp2, good_matches)
+
+    H, mask = find_homography(src_kp, dst_kp)
+
+    transformed_image = get_transformed_image(image2, H, final_width)
+
+    return transformed_image
+
+def get_panorama_image(w, h, homography_images):
+
+    l = len(homography_images)
+
+    prev_stitched_image = homography_images[0]
+
+    for image_index in range(1, l):
+        homography_images[image_index][0:h, w:] = prev_stitched_image
+
+        prev_stitched_image = homography_images[image_index]
+
+        cv2.imshow("Prev", prev_stitched_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    return prev_stitched_image
+
+def main():
+    folder_path = input("Enter folder path for panorama:")
+
+    files_path = get_files_list(folder_path, ".png")
+
+    if not files_path:
+        return print("Input folder is empty")
+
+    first_image = load_image(files_path[0], True, resize_factor)
+    homography_images = [first_image]
+
+    h, w = first_image.shape[:2]
+
+    final_width = w
+    final_height = h
+    total_images = len(files_path)
+
+    for image_index in range(1, total_images):
+        final_width += w
+
+        curr_img_path = files_path[image_index]
+        transformed_image = get_homography_image(homography_images[-1], curr_img_path, final_width)
+        
+        # show_image(transformed_image, "Transformed" + str(image_index))
+        # show_image(homography_images[-1], "Previous")
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        print(f"Stitched image {image_index} with {image_index+1}")
+
+        homography_images.append(transformed_image)
+
+    panorama_image = get_panorama_image(w, h, homography_images)
+
+    show_image(panorama_image, "Panorama image")
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
