f44d71c6850b0a40cccacfdd4453d1da7de0d73c
Naitik Soni <naitiksoni1705@gmail.com>
2025-12-17 10:50:10 +0530

    Added image for blob detection and modify some code

--- day-17/blob_detector.py
+++ day-17/blob_detector.py
@@ -1,24 +1,27 @@
 import cv2
 
-image_path = r"C:\Users\baps\Documents\Naitik Soni\ComputerVision\CV-30-day-challenge\Practical tasks day (1-7)\Task-5\coins-1.jpg"
-img = cv2.imread(image_path)
+image_path = r"C:\Users\baps\Documents\Naitik Soni\ComputerVision\CV-30-day-challenge\Images\night_sky.jpg"
 
+image = cv2.imread(image_path)
+
+img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 params = cv2.SimpleBlobDetector_Params()
 params.filterByArea = True
-params.minArea = 100
+params.minArea = 0.1
+params.maxArea = 5000
+
 params.filterByCircularity = True
-params.minCircularity = 0.7
+params.minCircularity = 0.1
 
 detector = cv2.SimpleBlobDetector_create(params)
 keypoints = detector.detect(img)
 
-out = cv2.drawKeypoints(img, keypoints, None,
-                         flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+img_with_blobs = cv2.drawKeypoints(
+    image, keypoints, None,
+    (0,255,0),
+    cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
+)
 
-
-cv2.imshow("Original image", img)
-cv2.imshow("Detected blob image", out)
-
+cv2.imshow("Blobs", img_with_blobs)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
